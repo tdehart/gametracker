3a8ae5ce6a96b2544ec40f5e630cf6d7a474dd0f
Tom DeHart <tdehart@gmail.com>
2012-04-04 11:07:27 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gametracker::Application.config.secret_token = '551f2f5b86b2bf62c311dd0e511a2c926510a92fa23d0e1317afcb297e98935fccb7092a17c70ec110cd54cfb6f49bd016688a7fb7a12d5b2bf3369c721fe0d4'
