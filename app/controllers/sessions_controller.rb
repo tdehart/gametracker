aaa7bc5ba9e98a8a9fa375e6da7ab95bfe0e57a2
Tom DeHart <tdehart@gmail.com>
2012-06-17 14:17:09 -0400

    Added friendly redirects

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,9 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to root_path
+      redirect_back_or root_path
     else
-      #flash.now[:error] = 'Invalid email/password combination'
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
