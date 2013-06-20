4cecf899a85fc460745f973a3dc656a0d04297b2
Tom DeHart <tdehart@gmail.com>
2013-06-19 22:04:49 -0400

    Removed old contributions migrations

--- db/migrate/20130609220538_rename_contributions_to_tournament_contributions.rb
+++
@@ -1,5 +0,0 @@
-class RenameContributionsToTournamentContributions < ActiveRecord::Migration
-  def change
-    rename_table :contributions, :tournament_contributions
-  end
-end
--- db/migrate/20130609222708_add_submitter_to_tournament_contributions.rb
+++
@@ -1,5 +0,0 @@
-class AddSubmitterToTournamentContributions < ActiveRecord::Migration
-  def change
-    add_column :tournament_contributions, :submitter, :boolean
-  end
-end
