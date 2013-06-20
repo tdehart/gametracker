03682f8364d20886c53d0449059f8e2ed126a26b
Tom DeHart <tdehart@gmail.com>
2013-06-20 07:19:19 -0400

    cleaned up image_uploader and added a cache dir

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,13 +1,7 @@
 # encoding: utf-8
 
 class ImageUploader < CarrierWave::Uploader::Base
-
-  # Include RMagick or MiniMagick support:
   include CarrierWave::RMagick
-
-  # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-  # include Sprockets::Helpers::RailsHelper
-  # include Sprockets::Helpers::IsolatedHelper
 
   if Rails.env.production?
     storage :fog
@@ -26,21 +20,18 @@ class ImageUploader < CarrierWave::Uploader::Base
     config.fog_directory  = 'gametracker' 
   end
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
+
   def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #    asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
       "/assets/" + [version_name, "placeholder.png"].compact.join('_')
   end
 
-  # Create different versions of your uploaded files:
   version :large_thumb do
     process :resize_to_fit => [64, 64]
   end
@@ -53,16 +44,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fit => [16, 16]
   end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
   def extension_white_list
     %w(jpg jpeg gif png)
   end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
-
 end
