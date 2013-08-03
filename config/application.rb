bf32be5361ccada069139c8a5f607835f32a8068
Tom DeHart <tdehart@gmail.com>
2013-08-03 15:36:19 -0400

    Added utf-8 encoding to application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,9 @@ module Gametracker
     config.time_zone = 'Eastern Time (US & Canada)'
     config.active_record.default_timezone = :local
 
+    #Need utf-8 for ruby-money
+    config.encoding = 'utf-8'
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
