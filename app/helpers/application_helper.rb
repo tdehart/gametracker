76d4750ccbdd128b54aa291ddc5adfe9eaad5d22
Tom DeHart <tdehart@gmail.com>
2013-08-08 20:23:20 -0400

    Updated landing background images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def randomized_background_image
     images = ["backgrounds/1.jpg", "backgrounds/2.jpg", "backgrounds/3.jpg", 
               "backgrounds/4.jpg", "backgrounds/5.jpg", "backgrounds/6.jpg",
-              "backgrounds/7.jpg", "backgrounds/8.jpg", "backgrounds/9.jpg"]
+              "backgrounds/7.jpg"]
     images[rand(images.size)]
   end
 
